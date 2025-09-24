98737cd8d46262f568302f8a221022228729fa75
David Gómez-Castro <david@gomezcastro.xyz>
2025-09-24 10:17:19 +0200

    general distance

--- scripts/simulation.jl
+++ scripts/simulation.jl
@@ -8,9 +8,9 @@ b = 1.
 f(x) = 1.0
 u(x) = (1-x^2)^s * gamma(1/2) / (4^s*gamma((1+2*s)/2)*gamma(1+s))
 
-h = 2. ^-3
+h = 2. ^-2
 ρ = 2. ^-6
-basis = WFEMIntervalBasis(a, b, h, s)
+basis = WFEMIntervalBasis(a, b, h, s, dist=x->max(1-x^4,0))
 quad = Quadrature1dHsNorm(a, b, s, ρ)
 prob = FractionalLaplaceInterval(a,b,s,f; basis=basis,quad=quad)
 @time uh = solve(prob)
