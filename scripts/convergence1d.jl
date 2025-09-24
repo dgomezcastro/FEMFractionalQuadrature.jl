55142fbebbb0300b26ded86917bc0beb0da8b467
David Gómez-Castro <david@gomezcastro.xyz>
2025-09-24 14:47:20 +0200

    convergence 1d on a loop

--- scripts/convergence1d.jl
+++ scripts/convergence1d.jl
@@ -4,27 +4,31 @@ using Plots, LaTeXStrings, SpecialFunctions
 using Base.Threads
 @show nthreads()
 
-s = 0.8
+
 a = -1.
 b = 1.
 
 f(x) = 1.0
-u(x) = max(1 - x^2, 0.0)^s * gamma(1 / 2) / (4^s * gamma((1 + 2 * s) / 2) * gamma(1 + s))
 
-hs = 2. .^ -(2:4)
-errs = zeros(length(hs))
+for s in 0.2:0.2:0.8
+    @show s
+    u(x) = max(1 - x^2, 0.0)^s * gamma(1 / 2) / (4^s * gamma((1 + 2 * s) / 2) * gamma(1 + s))
+    hs = 2. .^ -(2:5)
+    errs = zeros(length(hs))
 
-quad_fine = Quadrature1dHsNorm(a, b, s, hs[end]^3)
-for (i, h) in enumerate(hs)
-    ρ = h^3
-    basis = WFEMIntervalBasis(a, b, h, s, dist=x -> max(1 - x^4, 0))
+    ρ = 2^-11
+    @show ρ
     quad = Quadrature1dHsNorm(a, b, s, ρ)
-    prob = FractionalLaplaceInterval(a, b, s, f; basis=basis, quad=quad)
-    uh = solve(prob)
-    errs[i] = Hsseminorm(quad_fine, x -> u(x) - uh(x))
-end
-plot(hs, errs, marker=:circle, xscale=:log10, yscale=:log10, label="Error", xlabel=L"h", ylabel=L"[u - u_h]_{H^s}")
-plot!(hs, hs .^ (2 - s), label=L"h^{2-s}", linestyle=:dash)
-title!(latexstring("\\Omega = (-1,1), s=$s"))
+    for (i, h) in enumerate(hs)
+        @show i / length(hs)
+        basis = WFEMIntervalBasis(a, b, h, s, dist=x -> max(1 - x^4, 0))
+        prob = FractionalLaplaceInterval(a, b, s, f; basis=basis, quad=quad)
+        uh = solve(prob)
+        errs[i] = Hsseminorm(quad, x -> u(x) - uh(x))
+    end
+    plot(hs, errs, marker=:circle, xscale=:log10, yscale=:log10, label="Error", xlabel=L"h", ylabel=L"[u - u_h]_{H^s}")
+    plot!(hs, hs .^ (2 - s), label=L"h^{2-s}", linestyle=:dash)
+    title!(latexstring("\\Omega = (-1,1), s=$s, ρ=$ρ"))
 
-savefig("figs/convergence1d_s$(Int(10s)).pdf")
+    savefig("figs/convergence1d_s$(s)_rho$(ρ).pdf")
+end
