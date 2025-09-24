c6f44ef5f4f2f3ad273d693a7612c31f62ec863e
David Gómez-Castro <david@gomezcastro.xyz>
2025-09-24 10:15:26 +0200

    comparison with exact solution

--- scripts/simulation.jl
+++ scripts/simulation.jl
@@ -1,21 +1,21 @@
 using FEMFractionalQuadrature
-using Plots, LaTeXStrings
+using Plots, LaTeXStrings, SpecialFunctions
 
 s = 0.6
-h = 2. ^-3
-ρ = 2. ^-6
 a = -1.
 b = 1.
 
+f(x) = 1.0
+u(x) = (1-x^2)^s * gamma(1/2) / (4^s*gamma((1+2*s)/2)*gamma(1+s))
+
+h = 2. ^-3
+ρ = 2. ^-6
 basis = WFEMIntervalBasis(a, b, h, s)
 quad = Quadrature1dHsNorm(a, b, s, ρ)
-
-f(x) = 1.0
-
 prob = FractionalLaplaceInterval(a,b,s,f; basis=basis,quad=quad)
-
-@time u = solve(prob)
+@time uh = solve(prob)
 
 x = -1:1e-5:1
-plot(x, u.(x), label=L"u_h")
+plot(x,u.(x), label=L"u")
+plot!(x, uh.(x), label=L"u_h")
 scatter!(basis.mesh, zeros(size(basis.mesh)), label="")
