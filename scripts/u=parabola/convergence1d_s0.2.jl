ffd1d191d5756437ddea2e34708cb47e449468fa
Stefano Fronzoni <stefano.fronzoni@stx.ox.ac.uk>
2025-10-10 11:36:07 +0100

    setting right value of hs for u=parabola

--- scripts/u=parabola/convergence1d_s0.2.jl
+++ scripts/u=parabola/convergence1d_s0.2.jl
@@ -1,7 +1,12 @@
 include("convergence1d_function.jl")
 s = 0.2
-hs = 2. .^ -(1:4)
 
-ρs = [2^-11, 2^-11, 2^-11, 2^-14]
+ε = 1e-10
+a = -1. + ε
+b = 1. - ε
+Ns = 2. .^ (1:4)
+hs = ((b - a) / 2) ./ Ns
+
+ρs = [2^-11, 2^-11, 2^-12, 2^-14]
 
 convergence1d(s, hs, ρs)
--- scripts/u=parabola/convergence1d_s0.6.jl
+++ scripts/u=parabola/convergence1d_s0.6.jl
@@ -1,7 +1,12 @@
 include("convergence1d_function.jl")
 s = 0.6
-hs = 2. .^ -(1:4)
 
-ρs = [2^-12, 2^-12, 2^-14, 2^-15]
+ε = 1e-10
+a = -1. + ε
+b = 1. - ε
+Ns = 2. .^ (1:4)
+hs = ((b - a) / 2) ./ Ns
+
+ρs = [2^-12, 2^-12, 2^-15, 2^-16]
 
 convergence1d(s, hs, ρs)
