aec5cccab0b5a43651c9f33dc264220245fcdeef
Stefano Fronzoni <stefano.fronzoni@stx.ox.ac.uk>
2025-10-13 10:18:14 +0100

    plots

--- scripts/u=parabola/errors_Hs_plot.jl
+++ scripts/u=parabola/errors_Hs_plot.jl
@@ -3,10 +3,12 @@ using LinearAlgebra, LaTeXStrings, Plots, SpecialFunctions, JLD2, Plots.Measures
 
 dist_p = 4
 
-ss = [0.1, 0.2, 0.4, 0.6, 0.7]
+ss = [0.1, 0.2, 0.4, 0.6]
 
 logocolors = Colors.JULIA_LOGO_COLORS
 COLORS = ["orange", "royalblue1", "mediumorchid", "green4", "red2"]
+
+slopes = zeros(3, length(ss))
 
 global plt = plot(
     size=(1300, 1000),
@@ -24,6 +26,10 @@ for (k, s) in enumerate(ss)
     hs = dict["hs"]
     errsHs = dict["errsHs"]
 
+    for j in 1:length(hs)-1
+        slopes[j, k] = (log(errsHs[j+1]) - log(errsHs[j])) / (log(hs[j+1]) - log(hs[j]))
+    end
+
     plot!(plt, hs, errsHs, label=latexstring("s=") * "$s", color=COLORS[k], marker=:circle, markersize=8, markerstrokewidth=0)
     cs = (errsHs) ./ (hs .^ (3 / 2 - s))
     c = sum(cs) / length(cs)
@@ -35,4 +41,6 @@ end
 xlabel!(plt, L"h")
 ylabel!(plt, L"[u_h - u^\star]_{H^s(\mathbb{R})}")
 savefig(plt, "figs/u=parabola_ConvWFEM_Hs_compare.pdf")
+
+display(slopes)
 
