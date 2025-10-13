688f73750bf658bd3f63abfc003062c82618104d
Stefano Fronzoni <stefano.fronzoni@stx.ox.ac.uk>
2025-10-13 09:55:53 +0100

    plots sugar

--- scripts/f=1/errors_L2_plot.jl
+++ scripts/f=1/errors_L2_plot.jl
@@ -1,95 +1,38 @@
-# TODO This file should be like the Hs
 
-using FEMFractionalQuadrature
-using LinearAlgebra, Plots, Printf, SpecialFunctions, LaTeXStrings, Interpolations, Printf, DelimitedFiles, FilePathsBase, Measures, JLD2
-
-function FEM_piece(xi::Float64, x::Float64, h::Float64)
-    if abs(x .- xi) > h
-        return 0.0
-    else
-        return (1.0 - abs(x - xi) / h)
-    end
-end
-
-
-logocolors = Colors.JULIA_LOGO_COLORS
-H = [0.5, 0.250, 0.125, 0.0625]
-S = [0.1, 0.2, 0.4, 0.6]
-COLORS = ["orange", "royalblue1", "mediumorchid", "green4", "red2"]
-
-a = -1.
-b = 1.
-
-global ρ = 2^(-14) #ρ value (global because it gets changed in local scope)
-
-h_fine = 0.015625
-xx = collect(Float64, a:h_fine:b) #finer mesh
-NN = length(xx)
+using LinearAlgebra, LaTeXStrings, Plots, SpecialFunctions, JLD2, Plots.Measures, Printf
 
 dist_p = 4
 
-global plt = plot()
-g_fs = 30
-l_fs = 20
-t_fs = 20
+ss = [0.1, 0.2, 0.4, 0.6]
 
-global k = 1
-for s in S
+logocolors = Colors.JULIA_LOGO_COLORS
+COLORS = ["orange", "royalblue1", "mediumorchid", "green4", "red2"]
 
-
-    if s == 0.6
-        global ρ = 2^(-16)
-    end
-
-    if s == 0.7
-        global ρ = 2^(-16)
-    end
+global plt = plot(
+    size=(1300, 1000),
+    legend=:bottomright,
+    margin=10mm,
+    legendfontsize=20,
+    guidefontsize=30,
+    tickfontsize=20,
+    xscale=:log10,
+    yscale=:log10)
 
 
+for (k, s) in enumerate(ss)
+    filename = "figs/f=1_distancep$(dist_p)_s_$(s).jld2"
+    dict = load(filename)
+    hs = dict["hs"]
+    errsHs = dict["errsL2"]
+    hs_extended = sort(hs)
+    hs_extended = [0.9 * minimum(hs); hs_extended; 1.1 * maximum(hs)]
 
-    ERR_L2 = zeros(length(H))
-
-
-    for i in 1:length(H)
-
-        h = H[i]
-        basis = WFEMIntervalBasis(a, b, h, s, distance_power=4)
-        mesh = basis.mesh
-        filename = "save/Coefficients_s$(s)/convergence1d_distancep$(dist_p)_rho$(ρ)_s$(s)_solution_coefficients_h$(h).jld2"
-        dict = load(filename)
-        U = dict["U_coeff"]
-
-        uh = FractionalLaplaceIntervalSolution(basis, U)
-
-        uh_eval(x) = sum(uh.coeffs[i] * uh.basis(i, x) for i = 1:dimension(uh.basis))
-
-        ERR_L2[i] = L2norm_Simpson(a, b, x -> (sol(x) - uh_eval(x)), Δx=ρ)
-
-
-    end
-
-    if s == 0.1
-        c = (ERR_L2)[1] / (H .^ (2))[1]
-        plot!(plt, (H), H .^ (2) * c / 2, size=(1300, 1000), tickfontsize=t_fs, guidefontsize=g_fs, legendfontsize=l_fs,
-            linestyle=:dash, label=L"c h^2", color="black", margin=10mm,
-            xscale=:log10, yscale=:log10)
-    end
-
-    plot!(plt, H, (ERR_L2), size=(1300, 1000), legendfontsize=l_fs,
-        guidefontsize=g_fs, tickfontsize=t_fs, marker=:circle, markersize=8, markerstrokewidth=0, legend=:bottomright,
-        label=L"s=" * "$(s)", margin=10mm,
-        xscale=:log10, yscale=:log10, color=COLORS[k])
-
-    exp = 2 - s + s * (4 - 2 * s) / (5)
-    c = (ERR_L2)[1] / (H .^ (exp))[1]
-    plot!(plt, (H), H .^ (exp) * c, tickfontsize=t_fs, guidefontsize=g_fs, legendfontsize=l_fs,
-        linestyle=:dash, label=" ", color=COLORS[k],
-        xscale=:log10, yscale=:log10, margin=10mm)
-    xlabel!(plt, L"h", guidefontsize=g_fs)
-    ylabel!(plt, L"||u^{WFEM}-u^\ast||_{L^2(\Omega)}", guidefontsize=g_fs)
-
-    global k = k + 1
-
+    plot!(plt, hs, errsHs, label=latexstring("s=") * "$s", color=COLORS[k], marker=:circle, markersize=8, markerstrokewidth=0)
+    cs = (errsHs) ./ (hs .^ (2))
+    c = sum(cs) / length(cs)
+    plot!(plt, hs_extended, c * hs_extended .^ 2, label="", linestyle=:dash, color=COLORS[k])
 end
 
-savefig(plt, @sprintf("plots/ConvWFEM_L2_compare.pdf"))
+xlabel!(plt, L"h")
+ylabel!(plt, L"||u_h - u^\star||_{L^2(\Omega)}")
+savefig(plt, "figs/f=1_ConvWFEM_L2_compare.pdf")
--- scripts/u=parabola/errors_L2_plot.jl
+++ scripts/u=parabola/errors_L2_plot.jl
@@ -3,7 +3,7 @@ using LinearAlgebra, LaTeXStrings, Plots, SpecialFunctions, JLD2, Plots.Measures
 
 dist_p = 4
 
-ss = [0.1, 0.2, 0.4, 0.6, 0.7]
+ss = [0.1, 0.2, 0.4, 0.6]
 
 logocolors = Colors.JULIA_LOGO_COLORS
 COLORS = ["orange", "royalblue1", "mediumorchid", "green4", "red2"]
@@ -18,21 +18,22 @@ global plt = plot(
     xscale=:log10,
     yscale=:log10)
 
+
 for (k, s) in enumerate(ss)
     filename = "figs/u=parabola_distancep$(dist_p)_s_$(s).jld2"
     dict = load(filename)
     hs = dict["hs"]
-    errsHs = dict["errsHs"]
-
-    plot!(plt, hs, errsHs, label=latexstring("s=") * "$s", color=COLORS[k], marker=:circle, markersize=8, markerstrokewidth=0)
-    cs = (errsHs) ./ (hs .^ (3 / 2 - s))
-    c = sum(cs) / length(cs)
+    errsHs = dict["errsL2"]
     hs_extended = sort(hs)
     hs_extended = [0.9 * minimum(hs); hs_extended; 1.1 * maximum(hs)]
-    plot!(plt, hs_extended, c * hs_extended .^ (3 / 2 - s), label="", linestyle=:dash, color=COLORS[k])
+
+    exp = 3 / 2
+    plot!(plt, hs, errsHs, label=latexstring("s=") * "$s", color=COLORS[k], marker=:circle, markersize=8, markerstrokewidth=0)
+    cs = (errsHs) ./ (hs .^ (3 / 2))
+    c = sum(cs) / length(cs)
+    plot!(plt, hs_extended, c * hs_extended .^ (exp), label="", linestyle=:dash, color=COLORS[k])
 end
 
 xlabel!(plt, L"h")
-ylabel!(plt, L"[u_h - u^\star]_{H^s(\mathbb{R})}")
-savefig(plt, "figs/u=parabola_ConvWFEM_Hs_compare.pdf")
-
+ylabel!(plt, L"||u_h - u^\star||_{L^2(\Omega)}")
+savefig(plt, "figs/u=parabola_ConvWFEM_L2_compare.pdf")
