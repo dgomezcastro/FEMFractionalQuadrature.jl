50d3ea82f6b4c302280455d2b045aa8ebfce2e55
David Gómez-Castro <david@gomezcastro.xyz>
2025-11-07 11:07:38 +0100

    simplified 2d assemble

--- src/2d/assemble.jl
+++ src/2d/assemble.jl
@@ -1,13 +1,8 @@
 using SparseArrays, Plots, SpecialFunctions
 
 function evaluation(basis::AbstractFEM2dBasis, quad::Quadrature2dHsNorm)
-    basisdim = dimension(basis)
-
-    xx = quad.domain_quad[1, :]
-    yy = quad.domain_quad[2, :]
-
-    ξ = zeros(basisdim, npoints(quad))
-    @threads for i in range(1, basisdim)
+    ξ = zeros(dimension(basis), npoints(quad))
+    @threads for i in 1:dimension(basis)
         ξ[i, :] = [basis(i, quad.domain_quad[:, k]) for k in 1:npoints(quad)]
     end
 
@@ -15,19 +10,17 @@ function evaluation(basis::AbstractFEM2dBasis, quad::Quadrature2dHsNorm)
 end
 
 function assemble(basis::AbstractFEM2dBasis, quad::Quadrature2dHsNorm, f::Function)
-    basisdim = dimension(basis)
-
-    A = zeros(basisdim, basisdim)
+    A = zeros(dimension(basis), dimension(basis))
 
     ξ = evaluation(basis, quad)
 
-    @threads for i in range(1, basisdim)
+    @threads for i in 1:dimension(basis)
         φ_matrix = Matrix(reshape(ξ[i, :], Int64(sqrt(npoints(quad))), Int64(sqrt(npoints(quad))))')
         φ_convolved = convolve(quad.Kernel, φ_matrix) * quad.ρ^2
         φ_convolved = Matrix(reshape(φ_convolved', npoints(quad), 1))[:, 1]
-        for j in range(i, basisdim)
-            I1 = (quad.ρ^2 * quad.C_W * ξ[j, :])'ξ[i, :]
-            I2 = φ_convolved'ξ[j, :] * quad.ρ^2
+        for j in i:dimension(basis)
+            I1 = (quad.ρ^2 * quad.C_W * ξ[j, :])' * ξ[i, :]
+            I2 = φ_convolved' * ξ[j, :] * quad.ρ^2
             A[i, j] = 2 * (I1 - I2)
         end
     end
