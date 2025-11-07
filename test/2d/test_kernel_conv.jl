51c09ccabada1bcc3a45f4e48717e1528d78036f
David Gómez-Castro <david@gomezcastro.xyz>
2025-11-07 11:07:29 +0100

    tested kernel convolution with cuda

--- test/2d/test_kernel_conv.jl
+++ test/2d/test_kernel_conv.jl
@@ -1,6 +1,4 @@
-## TODO: THIS WILL BE TURNED INTO A TEST FOR 2D convolution
-
-using Test
+using Test, CUDA
 
 W(x, y) = 1 / (1 + x^2 + y^2)
 
@@ -62,6 +60,63 @@ b = 1.
                 end
             end
 
+        end
+    end
+end
+
+@testset "Test kernel convolution with CUDA" begin
+    L = b - a
+    if CUDA.functional()
+        N = 10
+        h = L / (N - 1)
+        @testset "N = $N, h = $h" begin
+
+            i1 = a / h
+            i2 = b / h
+
+            X = [i * h for i in i1:i2]
+            Y = [i * h for i in i1:i2]
+
+            X_W = [i * h for i in -L/h:L/h]
+            Y_W = [i * h for i in -L/h:L/h]
+
+            @testset "Domain sizes" begin
+                @test length(X) == N
+                @test length(Y) == N
+            end
+
+            φ_matrix = [φ(x, y) for x in X, y in Y]
+            W_matrix = [W(x, y) for x in X_W, y in Y_W]
+
+            K = FEMFractionalQuadrature.KernelFFT2D(W_matrix, (length(X), length(Y)); use_cuda=true)
+
+            φ_convolved = FEMFractionalQuadrature.convolve(K, φ_matrix)
+
+            @testset "Kernel Convolution basic properties" begin
+                @test size(φ_convolved) == size(φ_matrix)
+                @test all(isfinite, φ_convolved)
+                @test all(isreal, φ_convolved)
+            end
+
+            conv = zeros(size(φ_matrix))
+            for (i, x) in enumerate(X)
+                for (j, y) in enumerate(Y)
+                    for xi in X_W
+                        for yj in Y_W
+                            # @show i
+                            conv[i, j] += W(x - xi, y - yj) * φ(xi, yj)
+                        end
+                    end
+                end
+            end
+            φ_convolved = Array(φ_convolved)
+            @testset "Kernel Convolution correctness" begin
+                for i in 1:size(φ_matrix, 1)
+                    for j in 1:size(φ_matrix, 2)
+                        @test conv[i, j] ≈ φ_convolved[i, j] rtol = 1e-2
+                    end
+                end
+            end
         end
     end
 end
