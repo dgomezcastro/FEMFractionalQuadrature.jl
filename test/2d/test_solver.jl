3fc7e26dc644cd85adab83364020e021c6ffb768
David Gómez-Castro <david@gomezcastro.xyz>
2025-11-07 11:39:48 +0100

    Quad uses bounds

--- test/2d/test_solver.jl
+++ test/2d/test_solver.jl
@@ -8,16 +8,14 @@ using LinearAlgebra
 
     basis = FEMFractionalQuadrature.WFEMBasis2dDirichletUnitCircle(h, s;
         δ=P -> max(1 - norm(P)^2, 0.0))
-    # bounds = (-1.0, 1.0, -1.0, 1.0)
-    # quad = FEMFractionalQuadrature.Quadrature2dHsNorm(s, ρ, bounds)
-
-    quad = FEMFractionalQuadrature.Quadrature2dHsNorm(2., s, ρ)
+    bounds = (-1.0, 1.0, -1.0, 1.0)
+    quad = FEMFractionalQuadrature.Quadrature2dHsNorm(s, ρ, bounds)
 
     d = 2
     u(x) = max(1 - norm(x)^2, 0.0)^s * gamma(d / 2) / (4^s * gamma((d + 2 * s) / 2) * gamma(1 + s))
 
     f(x) = 1.0
-    uh = solve(f, basis, quad)
+    uh = FEMFractionalQuadrature.solve(f, basis, quad)
 
     us = [u(quad.domain_quad[:, k]) for k in 1:FEMFractionalQuadrature.npoints(quad)]
     uhs = [uh(quad.domain_quad[:, k]) for k in 1:FEMFractionalQuadrature.npoints(quad)]
