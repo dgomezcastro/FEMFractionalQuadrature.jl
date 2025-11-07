dc67fac57395ce1dc19e2c737e90af1660b857a4
David Gomez-Castro <david@gomezcastro.xyz>
2025-11-07 11:55:49 +0100

    Revert "Quad uses bounds"
    This reverts commit 3fc7e26dc644cd85adab83364020e021c6ffb768.

--- src/2d/Quad.jl
+++ src/2d/Quad.jl
@@ -10,32 +10,31 @@ struct Quadrature2dHsNorm <: AbstractQuadrature2dHsNorm
     s::Float64
     Cds::Float64
 
-    """
-    Construct a 2D quadrature for H^s norm computations.
-        Quadrature2dHsNorm(s::Float64, ρ::Float64, bounds::Tuple{Float64,Float64,Float64,Float64}; use_cuda::Bool=false)
-    s: fractional order
-    ρ: grid spacing
-    bounds: (x_min, x_max, y_min, y_max)
-    use_cuda: whether to use CUDA acceleration for FFT computations
-    """
-    function Quadrature2dHsNorm(s::Float64, ρ::Float64, bounds::Tuple{Float64,Float64,Float64,Float64})
+    function Quadrature2dHsNorm(diam::Float64, s::Float64, ρ::Float64)
         d = 2
-        Cds = (4^s * (gamma(d / 2 + s))) / (pi^(d / 2) * abs(gamma(-s)))
-
-        C_W = ρ^(-2 * s) * real(EpsteinLib.epsteinzeta(d + 2 * s; d=d))
 
         W_func(Px, Py) = Px^2 + Py^2 == 0 ? 0 : sqrt(Px^2 + Py^2)^(-d - 2 * s)
 
-        xmin, xmax, ymin, ymax = bounds
-        diam = sqrt((xmax - xmin)^2 + (ymax - ymin)^2)
         L = 1.5 * diam - ρ
+
+        i1 = (-L + ρ / 2) / ρ
+        i2 = (L - ρ / 2) / ρ
+
+        X = [i * ρ for i in i1:i2]
+        Y = [i * ρ for i in i1:i2]
+
+        X_W = [i * ρ for i in -L/ρ:L/ρ]
+        Y_W = [i * ρ for i in -L/ρ:L/ρ]
 
         domain_quad = generate_quadrature_2d(-L, L, ρ)
 
-        X_W = (i * ρ for i in -L/ρ:L/ρ)
-        W_FFT_Matrix = [W_func(x, y) for x in X_W, y in X_W]
+        W_FFT_Matrix = [W_func(x, y) for x in X_W, y in Y_W]
 
-        Kernel = KernelFFT2D(W_FFT_Matrix, size(domain_quad))
+        Kernel = KernelFFT2D(W_FFT_Matrix, (length(X), length(Y)))
+
+        C_W = ρ^(-2 * s) * real(EpsteinLib.epsteinzeta(d + 2 * s; d=d))
+
+        Cds = (4^s * (gamma(d / 2 + s))) / (pi^(d / 2) * abs(gamma(-s)))
 
         return new(domain_quad, ρ, C_W, Kernel, s, Cds)
     end
--- test/2d/test_solver.jl
+++ test/2d/test_solver.jl
@@ -8,14 +8,16 @@ using LinearAlgebra
 
     basis = FEMFractionalQuadrature.WFEMBasis2dDirichletUnitCircle(h, s;
         δ=P -> max(1 - norm(P)^2, 0.0))
-    bounds = (-1.0, 1.0, -1.0, 1.0)
-    quad = FEMFractionalQuadrature.Quadrature2dHsNorm(s, ρ, bounds)
+    # bounds = (-1.0, 1.0, -1.0, 1.0)
+    # quad = FEMFractionalQuadrature.Quadrature2dHsNorm(s, ρ, bounds)
+
+    quad = FEMFractionalQuadrature.Quadrature2dHsNorm(2., s, ρ)
 
     d = 2
     u(x) = max(1 - norm(x)^2, 0.0)^s * gamma(d / 2) / (4^s * gamma((d + 2 * s) / 2) * gamma(1 + s))
 
     f(x) = 1.0
-    uh = FEMFractionalQuadrature.solve(f, basis, quad)
+    uh = solve(f, basis, quad)
 
     us = [u(quad.domain_quad[:, k]) for k in 1:FEMFractionalQuadrature.npoints(quad)]
     uhs = [uh(quad.domain_quad[:, k]) for k in 1:FEMFractionalQuadrature.npoints(quad)]
