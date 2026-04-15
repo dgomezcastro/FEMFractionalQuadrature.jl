c21f3f4d211ce67f08e65c1592d159d0c27c6669
Stefano Fronzoni <stefano.fronzoni@stx.ox.ac.uk>
2026-04-15 16:24:19 +0100

    test example

--- scripts-2d/solve_and_plot.jl
+++ scripts-2d/solve_and_plot.jl
@@ -3,20 +3,20 @@ using CairoMakie
 
 println("Solving fractional Poisson problem in the unit circle")
 s = 0.7
-h = 2.0^-4
-ρ = 2.0^-7
+h = 2.0^-3
+ρ = 2.0^-6
 @show s, h, ρ
 
 f(x) = 1.0
 d = 2
 
-basis = FEMFractionalQuadrature.WFEMBasis2dDirichletUnitCircle(h, s;
+basis = FEMFractionalQuadrature.WFEMBasis2dDirichletUnitCircle_Square(h, s;
     δ=P -> max(1 - norm(P)^4, 0.0))
 bounds = (-1.0, 1.1, -1.0, 1.0)
 
 quad = FEMFractionalQuadrature.Quadrature2dHsNorm(s, ρ, bounds; use_cuda=false)
 
-@time uh = solve(f, basis, quad)
+@time uh = FEMFractionalQuadrature.solve_extranodes(f, basis, quad)
 
 # Plot
 using Triangulate, ExtendableGrids, GridVisualize, SimplexGridFactory, CairoMakie
@@ -30,8 +30,13 @@ Y = unique!(last.(quad.domain_quad[1, :]))
 # uhs_nan(x) = (basis.δ(x) > 1e-5) ? uh(x) : Float64(NaN)
 uhs = [uh([x, y]) for x in X, y in Y]
 
+fig = Figure(size=(1200, 800))
+ax = Axis3(fig[1, 1])
+
 # This takes forever
-surface(X, Y, uhs)
+surface!(ax, X, Y, uhs)
+
+CairoMakie.save("figs/surface_extranodes.png", fig)
 
 using SpecialFunctions
 d = 2
