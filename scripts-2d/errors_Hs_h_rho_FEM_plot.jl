2ed9ff3002b1132fe4bba361c5460c028ba99284
Stefano Fronzoni <stefano.fronzoni@stx.ox.ac.uk>
2026-04-20 11:29:53 +0100

    plots errors

--- scripts-2d/errors_Hs_h_rho_FEM_plot.jl
+++ scripts-2d/errors_Hs_h_rho_FEM_plot.jl
@@ -5,18 +5,20 @@ dist_p = 4
 
 ss = [0.1, 0.2, 0.4, 0.6]
 
-ρs = [2^-9, 2^-10, 2^-11]
-rho_labels = ["2^{-9}", "2^{-10}", "2^{-11}"]
+ρs = [2^-7, 2^-8, 2^-9]
+rho_labels = ["2^{-7}", "2^{-8}", "2^{-9}"]
+
 
 logocolors = Colors.JULIA_LOGO_COLORS
 COLORS = ["orange", "royalblue1", "mediumorchid", "green4", "red2"]
 
 slopes = zeros(length(ρs), length(ss))
 
+plot_old = 0
 
 global plt = plot(
     size=(1300, 1000),
-    legend=:bottomleft,
+    legend=:bottomright,
     margin=10mm,
     legendfontsize=20,
     guidefontsize=30,
@@ -26,7 +28,7 @@ global plt = plot(
 
 for (k, s) in enumerate(ss)
     for (kk, ρ) in enumerate(ρs)
-        filename = "figs/2d_f=1_distancep4_s_$(s)/rho$(ρs[kk])_.jld2"
+        filename = "figs/2d_FEM_f=1_s_$(s)/rho$(ρs[kk])_.jld2"
         dict = load(filename)
         hs = dict["hs"]
         errsHs = dict["errsHs"]
@@ -43,17 +45,17 @@ for (k, s) in enumerate(ss)
         plot!(plt, hs, errsHs, color=COLORS[k], marker=:circle, markersize=8, markerstrokewidth=0, label="", xlims=xlims)
         annotate!(plt, hs[end] / 1.05, errsHs[end], text(latexstring("\\rho= ") * latexstring(rho_labels[kk]), 12, :right, COLORS[k]))
         if kk == length(ρs)
-            cs = (errsHs) ./ (hs .^ (2 - s))
+            cs = (errsHs) ./ (hs .^ (1 / 2))
             c = sum(cs) / length(cs)
             hs_extended = sort(hs)
             hs_extended = [0.9 * minimum(hs); hs_extended; 1.1 * maximum(hs)]
-            plot!(plt, hs_extended, 0.8 * c * hs_extended .^ (2 - s), linestyle=:dash, color=COLORS[k], label=latexstring("s=") * "$s")
+            plot!(plt, hs_extended, c * hs_extended .^ (1 / 2), linestyle=:dash, color=COLORS[k], label=latexstring("s=") * "$s")
         end
     end
 end
 
 xlabel!(plt, L"h")
 ylabel!(plt, L"(a_\rho(u_{h,\rho} - u^\star, u_{h,\rho} - u^\star))^{\frac{1}{2}}")
-savefig(plt, "figs/2d_f=1_ConvWFEM_Hs_h_rho_2.pdf")
+savefig(plt, "figs/2d_f=1_ConvFEM_Hs_h_rho.pdf")
 
 display(slopes)
--- scripts/f=1/errors_Hs_h_rho_FEM_plot.jl
+++ scripts/f=1/errors_Hs_h_rho_FEM_plot.jl
@@ -5,18 +5,17 @@ dist_p = 4
 
 ss = [0.1, 0.2, 0.4, 0.6]
 
-ρs = [2^-9, 2^-10, 2^-11]
-rho_labels = ["2^{-9}", "2^{-10}", "2^{-11}"]
+ρs = [2^-6, 2^-7, 2^-8]
+rho_labels = ["2^{-6}", "2^{-7}", "2^{-8}"]
 
 logocolors = Colors.JULIA_LOGO_COLORS
 COLORS = ["orange", "royalblue1", "mediumorchid", "green4", "red2"]
 
-slopes = zeros(length(ρs), length(ss))
-
+slopes = zeros(4, length(ss))
 
 global plt = plot(
     size=(1300, 1000),
-    legend=:bottomleft,
+    legend=:bottomright,
     margin=10mm,
     legendfontsize=20,
     guidefontsize=30,
@@ -26,12 +25,10 @@ global plt = plot(
 
 for (k, s) in enumerate(ss)
     for (kk, ρ) in enumerate(ρs)
-        filename = "figs/2d_f=1_distancep4_s_$(s)/rho$(ρs[kk])_.jld2"
+        filename = "figs/FEM_f=1_s_$(s)/rho$(ρs[kk])_.jld2"
         dict = load(filename)
         hs = dict["hs"]
         errsHs = dict["errsHs"]
-        errsHs = sqrt.(errsHs)
-        uh_coeffs = dict["uh_coeff"]
 
         dx = 0.025 * (maximum(hs) - minimum(hs))
         xlims = (minimum(hs) - dx, maximum(hs) + dx)
@@ -42,18 +39,18 @@ for (k, s) in enumerate(ss)
 
         plot!(plt, hs, errsHs, color=COLORS[k], marker=:circle, markersize=8, markerstrokewidth=0, label="", xlims=xlims)
         annotate!(plt, hs[end] / 1.05, errsHs[end], text(latexstring("\\rho= ") * latexstring(rho_labels[kk]), 12, :right, COLORS[k]))
-        if kk == length(ρs)
-            cs = (errsHs) ./ (hs .^ (2 - s))
+        if ρ == 2^-8
+            cs = (errsHs) ./ (hs .^ (1 / 2))
             c = sum(cs) / length(cs)
             hs_extended = sort(hs)
             hs_extended = [0.9 * minimum(hs); hs_extended; 1.1 * maximum(hs)]
-            plot!(plt, hs_extended, 0.8 * c * hs_extended .^ (2 - s), linestyle=:dash, color=COLORS[k], label=latexstring("s=") * "$s")
+            plot!(plt, hs_extended, c * hs_extended .^ (1 / 2), linestyle=:dash, color=COLORS[k], label=latexstring("s=") * "$s")
         end
     end
 end
 
 xlabel!(plt, L"h")
 ylabel!(plt, L"(a_\rho(u_{h,\rho} - u^\star, u_{h,\rho} - u^\star))^{\frac{1}{2}}")
-savefig(plt, "figs/2d_f=1_ConvWFEM_Hs_h_rho_2.pdf")
+savefig(plt, "figs/f=1_ConvFEM_Hs_h_rho_compare.pdf")
 
 display(slopes)
